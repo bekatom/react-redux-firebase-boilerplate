41c629ab339c8799a365dd9320f435f2233c5fe7
Alexander Wong <admin@alexander-wong.com>
2016-09-08 13:32:16 -0600

    Removed iojs from travis.yml, added .editorconfig, minor semantic cleanup

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,7 +1,7 @@
-import { renderComponent , expect } from '../test_helper';
+import {renderComponent, expect} from '../test_helper';
 import App from '../../src/app/components/app';
 
-describe('App' , () => {
+describe('App', () => {
   let component;
 
   beforeEach(() => {
