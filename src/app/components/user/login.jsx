cd5496a66afd29915e3f2d2c31665b522b56cb95
Beka Tomashvili <bekatomashvili@gmail.com>
2016-11-16 03:57:13 +0400

    changed eslint config

--- src/app/components/user/login.jsx
+++ src/app/components/user/login.jsx
@@ -7,100 +7,102 @@ import { loginUser, fetchUser, loginWithProvider } from '../../actions/firebase_
 
 class UserLogin extends Component {
 
-  constructor(props) {
-    super(props);
-    this.onFormSubmit = this.onFormSubmit.bind(this);
-    this.loginWithProvider = this.loginWithProvider.bind(this);
-    this.state = {
-      message: '',
+    constructor(props) {
+      super(props);
+      this.onFormSubmit = this.onFormSubmit.bind(this);
+      this.loginWithProvider = this.loginWithProvider.bind(this);
+      this.state = {
+        message: '',
     };
   }
 
-  loginWithProvider(provider) {
-    this.props.loginWithProvider(provider).then(data => {
-      if (data.payload.errorCode)
-        this.setState({ message: data.payload.errorMessage })
-      else
-        browserHistory.push('/profile');
+    loginWithProvider(provider) {
+      this.props.loginWithProvider(provider).then((data) => {
+        if (data.payload.errorCode)
+          {this.setState({ message: data.payload.errorMessage })};
+        else
+        {browserHistory.push('/profile');}
     });
     // alert("login with provider");
   }
 
-  onFormSubmit(event) {
-    event.preventDefault();
+    onFormSubmit(event) {
+      event.preventDefault();
 
-    var email = this.refs.email.value;
-    var password = this.refs.password.value;
-    this.props.loginUser({ email: email, password: password }).then(data => {
-
-      if (data.payload.errorCode)
-        this.setState({ message: data.payload.errorMessage })
-      else
-        browserHistory.push('/profile');
-
+      let email = this.refs.email.value;
+      let password = this.refs.password.value;
+      this.props.loginUser({ email, password }).then((data) => {
+        if (data.payload.errorCode)
+          {this.setState({ message: data.payload.errorMessage })};
+        else
+        {browserHistory.push('/profile');}
     }
-    )
-
+    );
   }
 
-  render() {
-    return (
-      <div className="col-md-4">
-        <form id="frmLogin" role="form" onSubmit={this.onFormSubmit}>
-          <p>
-            {this.state.message}
+    render() {
+      return (
+        <div className="col-md-4">
+          <form id="frmLogin" role="form" onSubmit={this.onFormSubmit}>
+            <p>
+              {this.state.message}
           </p>
-          <h2>Login</h2>
-          <div className="form-group">
-            <label htmlFor="txtEmail">Email address</label>
-            <input type="email" className="form-control" id="txtEmail" ref="email" placeholder="Enter email"
-              name="email"/>
+            <h2>Login</h2>
+            <div className="form-group">
+              <label htmlFor="txtEmail">Email address</label>
+              <input type="email" className="form-control" id="txtEmail" ref="email" placeholder="Enter email"
+              name="email" 
+            />
           </div>
-          <div className="form-group">
-            <label htmlFor="txtPass">Password</label>
-            <input type="password" className="form-control" id="txtPass" ref="password" placeholder="Password"
-              name="password"/>
+            <div className="form-group">
+              <label htmlFor="txtPass">Password</label>
+              <input type="password" className="form-control" id="txtPass" ref="password" placeholder="Password"
+              name="password" 
+            />
           </div>
-          <button type="submit" className="btn btn-default btn-block">Login</button>
-          <br/>
-          <h5><Link to="/reset">Forgot password?</Link></h5>
+            <button type="submit" className="btn btn-default btn-block">Login</button>
+            <br />
+            <h5><Link to="/reset">Forgot password?</Link></h5>
 
-          <h4>Login with</h4>
-          <a href="#" className="btn btn-block btn-social btn-facebook" onClick={() => {
-            this.loginWithProvider("facebook")
-          } } data-provider="facebook">Facebook</a>
+            <h4>Login with</h4>
+            <a href="#" className="btn btn-block btn-social btn-facebook" onClick={() => {
+              this.loginWithProvider('facebook');
+          }} data-provider="facebook"
+          >Facebook</a>
 
-          <a href="#" className="btn btn-block btn-social btn-twitter" onClick={() => {
-            this.loginWithProvider("twitter")
-          } } data-provider="twitter">Twitter</a>
+            <a href="#" className="btn btn-block btn-social btn-twitter" onClick={() => {
+              this.loginWithProvider('twitter');
+          }} data-provider="twitter"
+          >Twitter</a>
 
-          <a href="#" className="btn btn-block btn-social btn-google" onClick={() => {
-            this.loginWithProvider("google")
-          } } data-provider="twitter">Google</a>
+            <a href="#" className="btn btn-block btn-social btn-google" onClick={() => {
+              this.loginWithProvider('google');
+          }} data-provider="twitter"
+          >Google</a>
 
-           <a href="#" className="btn btn-block btn-social btn-github" onClick={() => {
-            this.loginWithProvider("github")
-          } } data-provider="twitter">Github</a>
+            <a href="#" className="btn btn-block btn-social btn-github" onClick={() => {
+               this.loginWithProvider('github');
+           }} data-provider="twitter"
+           >Github</a>
 
         </form>
       </div>
 
-    )
+    );
   }
 
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({
-    loginUser,
-    fetchUser,
-    loginWithProvider
+    return bindActionCreators({
+      loginUser,
+      fetchUser,
+      loginWithProvider,
   }, dispatch);
 }
 
 function mapStateToProps(state) {
-  return { currentUser: state.currentUser };
-
+    return { currentUser: state.currentUser };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserLogin);
