c365b5d50817256fab29efc4f2b9224e0f9e5059
Ozzie Gooen <ozzieagooen@gmail.com>
2016-12-09 16:24:49 -0800

    Ensure that localStorage logic work if item is not first

--- src/app/utils/authenticated.js
+++ src/app/utils/authenticated.js
@@ -1,6 +1,6 @@
-
 function requireAuth(nextState, replace) {
-    const data = JSON.parse(localStorage.getItem(localStorage.key(0)));
+    const key = Object.keys(localStorage).find(e => e.match(/firebase:authUser/))
+    const data = JSON.parse(localStorage.getItem(key));
     if (!data.uid) {
         replace({
             pathname: '/login',
