7fd6056a01978a63c458a85cd47cb2148420cd2c
Beka Tomashvili <bekatomashvili@gmail.com>
2016-12-30 11:29:19 +0400

    fixed eslint stuff

--- src/app/components/app.jsx
+++ src/app/components/app.jsx
@@ -6,85 +6,89 @@ import { fetchUser, logoutUser } from '../actions/firebase_actions';
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.props.fetchUser();
-    this.logOut = this.logOut.bind(this);
-  }
+        this.props.fetchUser();
+        this.logOut = this.logOut.bind(this);
+    }
 
-  logOut() {
-    this.props.logoutUser().then(data=> {
+    logOut() {
+        this.props.logoutUser().then((data) => {
       // reload props from reducer
-      this.props.fetchUser();
-    });
-  }
+            this.props.fetchUser();
+        });
+    }
 
-  renderUserMenu(currentUser) {
+    renderUserMenu(currentUser) {
     // if current user exists and user id exists than make user navigation
-    if (currentUser && currentUser.uid)
-      return (
-        <li className="dropdown">
-          <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
-             aria-haspopup="true" aria-expanded="false">
-            {currentUser.email} <span className="caret"></span></a>
-          <ul className="dropdown-menu">
-            <li><Link to="/profile">Profile</Link></li>
-            <li role="separator" className="divider"></li>
-            <li><Link to="/logout" onClick={this.logOut}>Logout</Link></li>
-          </ul>
-        </li>
-      )
-    else
-      return [
-        <li key={1}><Link to="/login">Login</Link></li>,
-        <li key={2}><Link to="/register">Register</Link></li>
-      ]
+        if (currentUser && currentUser.uid) {
+            return (
+                <li className="dropdown">
+                    <a
+                      href="#" className="dropdown-toggle" data-toggle="dropdown" role="button"
+                      aria-haspopup="true" aria-expanded="false"
+                    >
+                        {currentUser.email} <span className="caret" /></a>
+                    <ul className="dropdown-menu">
+                        <li><Link to="/profile">Profile</Link></li>
+                        <li role="separator" className="divider" />
+                        <li><Link to="/logout" onClick={this.logOut}>Logout</Link></li>
+                    </ul>
+                </li>
+            );
+        } else {
+            return [
+                <li key={1}><Link to="/login">Login</Link></li>,
+                <li key={2}><Link to="/register">Register</Link></li>,
+            ];
+        }
+    }
 
-  }
+    render() {
+        return (
+            <div>
+                <header className="navbar navbar-static-top navbar-inverse" id="top" role="banner">
+                    <div className="container">
+                        <div className="navbar-header">
+                            <button
+                              className="navbar-toggle collapsed" type="button" data-toggle="collapse"
+                              data-target=".bs-navbar-collapse"
+                            ><span className="sr-only">Toggle navigation</span>
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                            </button>
+                            <Link to="/" className="navbar-brand">Firebase & Redux boilerplate</Link>
 
-  render() {
-    return (
-      <div>
-        <header className="navbar navbar-static-top navbar-inverse" id="top" role="banner">
-          <div className="container">
-            <div className="navbar-header">
-              <button className="navbar-toggle collapsed" type="button" data-toggle="collapse"
-                      data-target=".bs-navbar-collapse"><span className="sr-only">Toggle navigation</span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-                <span className="icon-bar"></span>
-              </button>
-              <Link to="/" className="navbar-brand">Firebase & Redux boilerplate</Link>
-
-            </div>
-            <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
-              <ul className="nav navbar-nav">
-                <li><Link to="/"> Home</Link></li>
+                        </div>
+                        <nav className="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+                            <ul className="nav navbar-nav">
+                                <li><Link to="/"> Home</Link></li>
                 ,
               </ul>
-              <ul className="nav navbar-nav navbar-right">
-                { this.renderUserMenu(this.props.currentUser) }
-              </ul>
-            </nav>
-          </div>
-        </header>
+                            <ul className="nav navbar-nav navbar-right">
+                                { this.renderUserMenu(this.props.currentUser) }
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
 
-        <div className="container">
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
+                <div className="container">
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ fetchUser, logoutUser }, dispatch);
+    return bindActionCreators({ fetchUser, logoutUser }, dispatch);
 }
 
 
 function mapStateToProps(state) {
-  return { currentUser: state.currentUser };
+    return { currentUser: state.currentUser };
 }
 
 
