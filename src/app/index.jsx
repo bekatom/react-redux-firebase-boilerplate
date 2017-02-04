318a4eea1d38c0c3e1210a4291927647ae27acf4
btomashvili <bekatomashvili@gmail.com>
2017-02-04 23:59:03 +0400

    eslint stuff

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,29 +1,33 @@
-import 'babel-polyfill';
-import 'bootstrap-social';
+/* eslint "import/imports-first": 0 */
+/* eslint no-undef: 0 */
+/* eslint import/extensions: 0 */
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+
+import 'babel-polyfill'
+import 'bootstrap-social'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 // import sagaMonitor from 'redux-saga/sagaMonitor';
 
-import { Router, browserHistory } from 'react-router';
-import ReduxPromise from 'redux-promise';
+import { Router, browserHistory } from 'react-router'
+// import ReduxPromise from 'redux-promise'
+
+import reducers from './reducers'
+import routes from './routes'
+import rootSaga from './sagas'
 
 
-import reducers from './reducers';
-import routes from './routes';
-import rootSaga from './sagas';
-
-
-const sagaMiddleware = createSagaMiddleware();
+const sagaMiddleware = createSagaMiddleware()
 const store = createStore(
   reducers,
   applyMiddleware(sagaMiddleware)
-);
+)
 
-sagaMiddleware.run(rootSaga);
+sagaMiddleware.run(rootSaga)
 // const action = type => store.dispatch({ type });
 
 
@@ -35,9 +39,9 @@ sagaMiddleware.run(rootSaga);
 
 
 // for bundling your styles
-import './bundle.scss';
+import './bundle.scss'
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+// const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore)
 
 
 /* function render() {
@@ -56,4 +60,5 @@ ReactDOM.render(
     <Provider store={store}>
         <Router history={browserHistory} routes={routes} />
     </Provider>
-  , document.querySelector('.react-root'));
+  , document.querySelector('.react-root'))
+
