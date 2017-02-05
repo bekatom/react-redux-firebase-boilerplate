130658f1fe0e472ca258a7ee2d94e3527d231c41
btomashvili <bekatomashvili@gmail.com>
2017-02-06 03:47:29 +0400

    userFetchSagas

--- src/app/sagas/index.js
+++ src/app/sagas/index.js
@@ -1,7 +1,7 @@
 import { delay } from 'redux-saga'
 import { put, call, takeEvery, takeLatest, fork } from 'redux-saga/effects'
 import startup from './startupSagas'
-import userSagas from './userAuth'
+import userSagas, { userFetchSagas } from './userAuth'
 import * as actionTypes from '../actions/types'
 // import { startupRequest, startupSuccess, startupFailure } from '../actions/startup'
 
@@ -38,6 +38,7 @@ export default function* root() {
         // takeEvery('INCREMENT_ASYNC', incrementAsync),
         takeLatest(actionTypes.STARTUP_REQUEST, startup),
         takeLatest(actionTypes.USER_LOGIN_REQUEST, userSagas),
+        takeLatest(actionTypes.USER_FETCH_REQUEST, userFetchSagas),
         // fork(testMethod),
     ]
 }
